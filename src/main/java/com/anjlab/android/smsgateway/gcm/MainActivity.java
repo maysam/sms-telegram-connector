0640c73b2d0f7f8bb48bf07d411f94fe2cfa78ec
Maysam Torabi <kherad@gmail.com>
2018-01-21 12:44:41 +0330

    sending received sms to telegram bot

--- src/main/java/com/anjlab/android/smsgateway/gcm/MainActivity.java
+++ src/main/java/com/anjlab/android/smsgateway/gcm/MainActivity.java
@@ -1,65 +1,65 @@
 package com.anjlab.android.smsgateway.gcm;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.os.AsyncTask;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.TextView;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.gcm.GoogleCloudMessaging;
-
-import java.io.IOException;
-
+import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.InjectView;
+
+import static android.Manifest.permission.READ_CONTACTS;
+import static android.Manifest.permission.RECEIVE_SMS;
 
 public class MainActivity extends Activity
 {
 
-    public static final String PROPERTY_REG_ID = "registration_id";
-    public static final String PROPERTY_SENDER_ID = "sender_id";
-    private static final String PROPERTY_APP_VERSION = "app_version";
-    public static final String PROPERTY_FORWARD_ENABLED = "forwarding_enabled";
-    public static final String PROPERTY_FORWARD_URL = "forwarding_url";
-    static final String TAG = "SMS Gateway";
+//    static final String TAG = "SMS Gateway";
+    private static final int MY_PERMISSIONS_REQUEST_READ_SMS = 314;
 
-    @InjectView(R.id.send)
-    Button send;
 
-    @InjectView(R.id.saveProjectId)
-    Button saveProjectId;
-
-    @InjectView(R.id.saveForwarning)
-    Button saveForwarning;
-
-    @InjectView(R.id.display)
-    TextView display;
-
-    @InjectView(R.id.senderIdInputView)
+    @BindView(R.id.senderIdInputView)
     EditText senderId;
 
-    @InjectView(R.id.forwardingUrlInputView)
+    @BindView(R.id.forwardingUrlInputView)
     EditText forwardingUrl;
 
-    @InjectView(R.id.forwardingEnabled)
+    @BindView(R.id.forwardingEnabled)
     CheckBox forwardingEnabled;
 
-    @InjectView(R.id.senderIdPanel)
+    @BindView(R.id.senderIdPanel)
     View senderIdPanel;
 
-    GoogleCloudMessaging gcm;
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_SMS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+//  public static final String RECEIVE_SMS = "android.permission.RECEIVE_SMS";
+//  public static final String READ_CONTACTS = "android.permission.RECEIVE_SMS";
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -69,190 +69,10 @@ public class MainActivity extends Activity
         setContentView(R.layout.main);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-        ButterKnife.inject(this);
-
-        send.setOnClickListener(new View.OnClickListener()
-        {
-
-            @Override
-            public void onClick(View v)
-            {
-                Intent intent = new Intent(Intent.ACTION_SEND);
-                intent.setType("text/plain");
-                intent.putExtra(Intent.EXTRA_SUBJECT, "Your Android SMS Gateway Registration ID");
-                intent.putExtra(Intent.EXTRA_TEXT, getRegistrationId());
-                startActivity(Intent.createChooser(intent, "Send Email"));
-            }
-        });
-        saveProjectId.setOnClickListener(new View.OnClickListener()
-        {
-
-            @Override
-            public void onClick(View v)
-            {
-                storeSenderId(senderId.getText().toString());
-                registerInBackground();
-            }
-        });
-        saveForwarning.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                getGcmPreferences()
-                        .edit()
-                        .putString(PROPERTY_FORWARD_URL, forwardingUrl.getText().toString())
-                        .putBoolean(PROPERTY_FORWARD_ENABLED, forwardingEnabled.isChecked())
-                        .apply();
-            }
-        });
-
-        forwardingUrl.setText(getForwardingUrl());
-        forwardingEnabled.setChecked(getForwardingEnabled());
-
-        // Check device for Play Services APK. If check succeeds, proceed with GCM registration.
-        if (checkPlayServices())
-        {
-            gcm = GoogleCloudMessaging.getInstance(this);
-            if (TextUtils.isEmpty(getRegistrationId()))
-            {
-                send.setVisibility(View.INVISIBLE);
-                display.setText("");
-            }
-            else
-                senderIdPanel.setVisibility(View.GONE);
-        }
-        else
-        {
-            display.setText(R.string.error_no_play_services);
-        }
-    }
-
-    @Override
-    protected void onResume()
-    {
-        super.onResume();
-
-        checkPlayServices();
-    }
-
-    private boolean checkPlayServices()
-    {
-        return GooglePlayServicesUtil.isGooglePlayServicesAvailable(this) == ConnectionResult.SUCCESS;
-    }
-
-    private void storeRegistrationId(String regId)
-    {
-        getGcmPreferences()
-                .edit()
-                .putString(PROPERTY_REG_ID, regId)
-                .putInt(PROPERTY_APP_VERSION, getAppVersion())
-                .apply();
-    }
-
-    private String getRegistrationId()
-    {
-        SharedPreferences prefs = getGcmPreferences();
-        String registrationId = prefs.getString(PROPERTY_REG_ID, "");
-        if (registrationId.length() == 0)
-        {
-            Log.i(TAG, "Registration not found.");
-            return "";
-        }
-        // Check if app was updated; if so, it must clear the registration ID
-        // since the existing regID is not guaranteed to work with the new
-        // app version.
-        int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION, Integer.MIN_VALUE);
-        if (registeredVersion != getAppVersion())
-        {
-            Log.i(TAG, "App version changed.");
-            return "";
-        }
-        return registrationId;
-    }
-
-    private String getSenderId()
-    {
-        return getGcmPreferences().getString(PROPERTY_SENDER_ID, "");
-    }
-
-    private void storeSenderId(String senderId)
-    {
-        getGcmPreferences()
-                .edit()
-                .putString(PROPERTY_SENDER_ID, senderId)
-                .apply();
-    }
-
-    private void registerInBackground()
-    {
-        new AsyncTask<Void, Void, String>()
-        {
-            @Override
-            protected String doInBackground(Void... params)
-            {
-                try
-                {
-                    if (gcm == null)
-                        gcm = GoogleCloudMessaging.getInstance(getApplicationContext());
-                    String id = gcm.register(getSenderId());
-                    storeRegistrationId(id);
-                    return "Device registered, Registration ID = " + id;
-                }
-                catch (IOException ex)
-                {
-                    return "Error: " + ex.getMessage();
-                }
-            }
-
-            @Override
-            protected void onPostExecute(final String msg)
-            {
-                runOnUiThread(new Runnable()
-                {
-
-                    @Override
-                    public void run()
-                    {
-                        display.setText(msg);
-                        if (!TextUtils.isEmpty(getRegistrationId()))
-                        {
-                            send.setVisibility(View.VISIBLE);
-                            senderIdPanel.setVisibility(View.GONE);
-                        }
-                    }
-                });
-            }
-        }.execute(null, null, null);
-    }
-
-    private int getAppVersion()
-    {
-        try
-        {
-            return getApplicationContext()
-                    .getPackageManager()
-                    .getPackageInfo(getApplicationContext().getPackageName(), 0)
-                    .versionCode;
-        }
-        catch (NameNotFoundException e)
-        {
-            throw new RuntimeException("Could not get package name: " + e);
-        }
-    }
-
-    private SharedPreferences getGcmPreferences()
-    {
-        return getSharedPreferences(getApplicationContext().getPackageName(), Context.MODE_PRIVATE);
-    }
-
-    private String getForwardingUrl()
-    {
-        return getGcmPreferences().getString(PROPERTY_FORWARD_URL, "");
-    }
-
-    private boolean getForwardingEnabled()
-    {
-        return getGcmPreferences().getBoolean(PROPERTY_FORWARD_ENABLED, false);
+        ButterKnife.bind(this);
+//        int permissionCheck = ContextCompat.checkSelfPermission(this, RECEIVE_SMS);
+        ActivityCompat.requestPermissions(this,
+                new String[]{RECEIVE_SMS, READ_CONTACTS},
+                MY_PERMISSIONS_REQUEST_READ_SMS);
     }
 }
--- src/main/java/com/anjlab/android/smsgateway/gcm/SmsBroadcastReceiver.java
+++ src/main/java/com/anjlab/android/smsgateway/gcm/SmsBroadcastReceiver.java
@@ -1,17 +1,95 @@
 package com.anjlab.android.smsgateway.gcm;
 
-import android.app.Activity;
-import android.content.ComponentName;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.content.WakefulBroadcastReceiver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.telephony.SmsMessage;
+import android.util.Log;
 
-public class SmsBroadcastReceiver extends WakefulBroadcastReceiver {
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Arrays;
 
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        ComponentName comp = new ComponentName(context.getPackageName(), SmsIntentService.class.getName());
-        startWakefulService(context, (intent.setComponent(comp)));
-        setResultCode(Activity.RESULT_OK);
-    }
+import static android.provider.Telephony.Sms.Intents.getMessagesFromIntent;
+
+public class SmsBroadcastReceiver extends BroadcastReceiver {
+
+  @Override
+  public void onReceive(Context context, Intent intent) {
+            /* Get Messages */
+    SmsMessage[] sms = getMessagesFromIntent(intent);
+    assert sms != null;
+
+
+    String urlStringFormat = "https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s";
+    String apiToken = "488699109:AAEKYpTzU6VNumJIAY_L9yEQZpZND7Da688";
+    String chatId = "59755972";
+    String sender = null;
+    StringBuilder msg = null;
+    String last_phone = null;
+
+    String urlString = null;
+    for (SmsMessage smsMessage : sms) {
+                /* Parse Each Message */
+      String phone = smsMessage.getOriginatingAddress();
+      String message = smsMessage.getDisplayMessageBody();
+      if (last_phone == null || !last_phone.contentEquals(phone)) {
+        if (last_phone != null && !last_phone.contentEquals(phone)) sendMessageToTelegram(urlString);
+        last_phone = phone;
+        msg = new StringBuilder(message);
+        Uri personUri = Uri.withAppendedPath( ContactsContract.PhoneLookup.CONTENT_FILTER_URI, phone);
+        Cursor cur = context.getContentResolver().query(personUri, new String[] { ContactsContract.PhoneLookup.DISPLAY_NAME }, null, null, null );
+        if( cur != null && cur.moveToFirst() ) {
+          int nameIndex = cur.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME);
+          sender = cur.getString(nameIndex);
+          cur.close();
+        } else {
+          sender = phone;
+        }
+      } else {
+        msg.append(message);
+      }
+      String text = sender + ":\n" + msg;
+      urlString = String.format(urlStringFormat, apiToken, chatId, text.replace("\n", "%0A"));
+    } // for
+    sendMessageToTelegram(urlString);
+  }
+
+  private void sendMessageToTelegram(final String urlString) {
+
+    Thread thread = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          URL url = new URL(urlString);
+          URLConnection conn = url.openConnection();
+          conn.getContent();
+//            StringBuilder sb = new StringBuilder();
+//            InputStream is = new BufferedInputStream(conn.getInputStream());
+//            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+//            String inputLine = "";
+//            while ((inputLine = br.readLine()) != null) {
+//              sb.append(inputLine);
+//            }
+//            String response = sb.toString();
+// Do what you want with response
+        } catch (MalformedURLException e) {
+          Log.e("SmsBroadcastReceiver", e.getMessage());
+          Log.e("SmsBroadcastReceiver", Arrays.toString(e.getStackTrace()));
+          e.printStackTrace();
+        } catch (IOException e) {
+          Log.e("SmsBroadcastReceiver", e.getMessage());
+          Log.e("SmsBroadcastReceiver", Arrays.toString(e.getStackTrace()));
+          e.printStackTrace();
+        }
+      }
+    });
+
+    thread.start();
+  }
 }
